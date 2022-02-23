c7ac024d573d2f988d6382395c78df64aa6d7288
vamshi-0305 <61814824+vamshi-0305@users.noreply.github.com>
2022-02-23 19:10:25 +0530

    Create DefaultInitialization.java

---
+++ java_assignments/Assignment5/DefaultInitialization.java
@@ -0,0 +1,21 @@
+package yourname.assignment.data;
+
+public class DefaultInitialization {
+    int a;
+    char c;
+    public void print(){
+        System.out.println(a+" "+c);
+    }
+    public void display(){
+        int b;
+        char d;
+        System.out.println(b+" "+d);
+    }
+//    public static void main(String[] args) {
+//        DefaultInitialization di=new DefaultInitialization();
+//        di.print();
+//    }
+}
+
+
+//local variables cannot be used before initialization as they cannot procure a default value similar to global variables
