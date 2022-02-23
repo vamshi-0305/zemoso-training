c53a3e49ed67c73471253c3e84cb59307e977bcf
vamshi-0305 <61814824+vamshi-0305@users.noreply.github.com>
2022-02-23 19:20:00 +0530

    Create Rodent.java

---
+++ java_assignments/Assignment7/rodent/Rodent.java
@@ -0,0 +1,19 @@
+package java_assignments.Assignment7;
+
+public class Rodent {
+
+     Rodent(){
+         System.out.println("Inside Rodent class");
+     }
+
+      void checkCheekPouch(){
+         System.out.println("Rodents usually don't have cheek pouches");
+      }
+
+    //abstract void lifeSpan();
+
+    void lifeSpan(){
+         System.out.println("Rodents live for at least a year");
+    }
+
+}
