446fd7d0c3ec705fd4cd238c5ef103fdc78bca43
vamshi-0305 <61814824+vamshi-0305@users.noreply.github.com>
2022-02-23 19:11:08 +0530

    Create main.java

---
+++ java_assignments/Assignment5/main.java
@@ -0,0 +1,12 @@
+package yourname.assignment.main;
+
+import yourname.assignment.data.DefaultInitialization;
+
+public class main {
+    public static void main(String[] args) {
+        DefaultInitialization a=new DefaultInitialization();
+        a.print();
+        a.display();
+
+    }
+}
