57dc9025b8e14a4d8ed82b103f90dbc51f2219d3
vamshi-0305 <61814824+vamshi-0305@users.noreply.github.com>
2022-02-23 19:28:18 +0530

    Create Main.java

---
+++ java_assignments/Assignment7/cycle/Main.java
@@ -0,0 +1,13 @@
+package java_assignments.Assignment7;
+
+public class Main {
+    public static void main(String[] args){
+
+        SubClass obj = new SubClass();
+        obj.interfaceOneMethod(obj);
+        obj.interfaceTwoMethod(obj);
+        obj.interfaceThreeMethod(obj);
+        obj.interfaceCombinedMethod(obj);
+
+    }
+}
